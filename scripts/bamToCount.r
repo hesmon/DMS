28763fcfa47decb8c99cc10e245e6a7ef877e074
Hesam Montazeri <hesam.montazeri@ut.ac.ir>
2021-08-06 19:09:47 +0430

    initial commit

---
+++ scripts/bamToCount.r
@@ -0,0 +1,37 @@
+rm(list=ls())
+source("scripts/bamToCountFunctions.r")
+# library(Rsamtools)
+library(GenomicAlignments)
+library(tidyverse)
+library(data.table)
+
+
+##First read in the arguments listed at the command line
+args=(commandArgs(TRUE))
+print(args)
+##args is now a list of character vectors
+## First check to see if arguments are passed.
+## Then cycle through each element of the list and evaluate the expressions.
+if(length(args)==0){
+  print("No arguments supplied.")
+}else{
+  for(i in 1:length(args)){
+    eval(parse(text=args[[i]]))
+  }
+}
+print(ls())
+print(i)
+print(bam_folder)
+print(Input_SortedBam)
+print(pathOut)
+print(st)
+print(end)
+print(ROI) # ROI is a vector containing the start and end from final sample .csv table (e.g. for se1: c(1, 13))
+print(set)
+print(replicate)
+print(includeFinalFlankingResid)
+oligo_9NN = readOligos()
+
+
+count_table(bam_folder, Input_SortedBam, pathOut, st, end, ROI, pathToRef,
+            set, replicate, includeFinalFlankingResid)
---
+++ scripts/bamToCountTest.r
@@ -0,0 +1,25 @@
+rm(list=ls())
+source("scripts/bamToCountFunctions.r")
+# library(Rsamtools)
+library(GenomicAlignments)
+library(tidyverse)
+library(data.table)
+
+oligo_9NN = readOligos()
+
+bam_folder = "../glu_ext/"
+Input_SortedBam = "Set15_rep1_sorted.bam"
+ROI = c(212:227)
+st = 212
+end = 227
+
+
+pathOut = "../csv3/"
+pathToRef <- "../Reference/Ref_3CL.fasta"
+set = "Set15"
+replicate=1
+includeFinalFlankingResid = FALSE
+
+
+count_table(bam_folder, Input_SortedBam, pathOut, st, end, ROI, pathToRef,
+            set, replicate, includeFinalFlankingResid)
